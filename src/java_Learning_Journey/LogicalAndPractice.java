50960273a205922fe47b5f348df3c22b49cf3f50
Shivanand Malkapuri <shivanandmalkapuri@gmail.com>
2026-07-08 22:32:15 +0530

    Implement Logical Operators

---
+++ src/java_Learning_Journey/LogicalAndPractice.java
@@ -0,0 +1,34 @@
+package java_Learning_Journey;
+
+public class LogicalAndPractice {
+
+	public static void main(String[] args) {
+//		int total = 1500;
+//		int quantity = 6;
+		
+//		if (total >= 1000 && quantity >=5) {
+//			System.out.println("Discount Approved");
+//		} 
+//		else {
+//			System.out.println("Discount Not Approved");
+//		}
+		
+//		if (total >= 1000 || quantity >= 5) {
+//			System.out.println("true");
+//		}
+//		else {
+//			System.out.println("false");
+//		}
+		
+		boolean isAdmin = false;
+		
+		if (!isAdmin) {
+			System.out.println("Access Not Granted");
+		}
+		else {
+			System.out.println("Access Granted");
+		}
+		
+	}
+
+}
